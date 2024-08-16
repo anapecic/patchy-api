7be15144dd1c58b2b09bd24318a3401ca85a6815
Ana Pecic <ana.pecic97@gmail.com>
2024-08-16 22:03:03 +0200

    final touches

--- pages/index.js
+++ pages/index.js
@@ -51,7 +51,21 @@ const StyledDomain = styled.div`
   position: relative;
 `;
 
-const patchys = getAllImages();
+const patchys = [
+  "/57F556A3-B8C5-4B88-BEDC-06D1461E6BBD.jpg",
+  "/IMG-20231102-WA0018.jpg",
+  "/IMG-20231207-WA0002.jpeg",
+  "/IMG-20231226-WA0003.jpeg",
+  "/IMG-20240118-WA0009.jpg",
+  "/IMG-20240122-WA0009.jpg",
+  "/IMG-20240123-WA0000.jpg",
+  "/IMG-20240123-WA0001.jpg",
+  "/IMG-20240123-WA0004.jpg",
+  "/IMG-20240123-WA0006.jpg",
+  "/IMG-20240123-WA0007.jpg",
+  "/IMG-20240123-WA0008.jpg",
+  "/IMG-20240123-WA0009.jpg",
+];
 // const fetcher = (...args) => fetch(...args).then((res) => res.json());
 
 export default function Home() {
